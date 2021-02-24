0c2edd917605aa59a8829211e3a30139105cc60d
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2021-02-23 19:40:19 -0500

    Moved main function from DCGANPT.py to main.py and added argument parsing

---
+++ main.py
@@ -0,0 +1,68 @@
+import sys
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+#import torch.nn.parallel
+#import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+from Utils import Utils
+from config import parse_args
+
+from models.gan import GAN
+from models.dcgan import DCGAN_MODEL
+from models.wgan_clipping import WGAN_CP
+from models.wgan_gradient_penalty import WGAN_GP
+
+# Reference: https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
+# Set random seed for reproducibility
+manualSeed = 999
+# manualSeed = random.randint(1, 10000) # to produce different results each time
+print("Random Seed: ", manualSeed)
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
+
+
+
+def main(args):
+    #--------------prepare data------------------------
+    model = None
+    if args.model == 'GAN':
+        model = GAN(args)
+    elif args.model == 'DCGAN':
+        model = DCGAN_MODEL(args)
+    elif args.model == 'WGAN-CP':
+        model = WGAN_CP(args)
+    elif args.model == 'WGAN-GP':
+        model = WGAN_GP(args)
+    else:
+        print("Model type non-existing. Try again.")
+        exit(-1)
+    utils = Utils()
+    dataset = args.dataset
+    dataroot = args.dataroot
+    batch_size = args.batch_size 
+    epochs = args.epochs
+    channels = args.channels
+    workers = 0  # number of workers for dataloader, 2 creates problems
+    train_loader, test_loader = utils.prepare_data(dataroot, batch_size, workers, dataset)
+    # Start model training
+    if args.is_train == 'True':
+        model.train(train_loader)
+
+    # start evaluating on test data
+    else:
+        model.evaluate(test_loader, args.load_D, args.load_G)
+        # for i in range(50):
+        #    model.generate_latent_walk(i)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    sys.exit(int(main(args) or 0))
