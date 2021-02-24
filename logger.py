8402208d33ac24af655a713099d08ffa3cad8ac6
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2021-02-23 19:43:08 -0500

    Added a simple tensorboard logger

---
+++ logger.py
@@ -0,0 +1,13 @@
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
+class Logger(object):
+    def __init__(self, log_dir):
+        """Create a summary writer logging to log_dir."""
+        self.writer = SummaryWriter(log_dir)
+
+    def log_loss(self, tag, loss, iteration):
+        self.writer.add_scalar(tag, loss, iteration)
+
+    def close(self):
+        self.writer.close()
